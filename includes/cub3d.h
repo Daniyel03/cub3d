5a305ce0955013c15091927775407c0113562352
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-06-28 14:16:34 +0200

    add keybing struct

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hannes <hrother@student.42vienna.com>      +#+  +:+       +#+        */
+/*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 17:06:12 by hrother           #+#    #+#             */
-/*   Updated: 2024/06/28 00:36:35 by hannes           ###   ########.fr       */
+/*   Updated: 2024/06/28 14:16:09 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,46 @@
 # include <X11/keysym.h>
 # include <math.h>
 # include <mlx.h>
-# include <stdlib.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 
 # define WIDTH 1280
 # define HEIGHT 720
 
+typedef void	(*t_key_func)(int *val, int rate);
+
+typedef struct s_keybind
+{
+	int			keycode;
+	int			pressed;
+	t_key_func	func;
+}				t_keybind;
+
 typedef struct t_img
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}			t_img;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_img;
 
 typedef struct s_map
 {
-	int		*fd;
-}			t_map;
+	int			*fd;
+}				t_map;
 
 typedef struct s_cb
 {
-	void	*mlx;
-	void	*win;
-	t_map	*map;
+	void		*mlx;
+	void		*win;
+	t_map		*map;
 	// t_key	*keys;
-	t_img	img;
+	t_img		img;
 	// t_pers	pers;
-} t_cb;
+}				t_cb;
 
-void	draw_map(t_cb *cb, int map[10][10], int y_max, int x_max);
+void			draw_map(t_cb *cb, int map[10][10], int y_max, int x_max);
 
 #endif
