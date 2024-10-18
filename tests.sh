b0a17cda349a03fe2df84e2ba79b764ebd940173
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-10-19 00:08:43 +0200

    first tests

--- tests.sh
+++ tests.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Hello tester"
+echo test with no args
+./cub3D
+if [ $? != 1 ]; then
+    echo failed
+    exit 1
+fi
+
+echo test with too many args
+./cub3D maps/example.cub maps/map.cub
+if [ $? != 1 ]; then
+    echo failed
+    exit 1
+fi
