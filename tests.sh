82e63f7d3cf533e3f2b3a9d5c34d524062d9eb9b
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-10-19 17:11:10 +0200

    valid and invalid map test

--- tests.sh
+++ tests.sh
@@ -7,9 +7,55 @@ if [ $? != 1 ]; then
     exit 1
 fi
 
+echo ----------
 echo test with too many args
 ./cub3D maps/example.cub maps/map.cub
 if [ $? != 1 ]; then
     echo failed
     exit 1
 fi
+
+for file in maps/valid/*; do
+    echo ----------
+    echo testsing $file
+    ./cub3D "$file" &
+    pid=$!
+    sleep 1
+    if kill -0 $pid 2>/dev/null; then
+        echo "cub3D is still running after 1 second. Terminating..."
+        kill $pid
+        wait $pid
+        EXIT_CODE=$?
+        echo "Success"
+    else
+        wait $pid
+        EXIT_CODE=$?
+        echo "\ncub3D exited early with exit code: $EXIT_CODE"
+        exit 1
+    fi
+done
+
+for file in maps/invalid/*; do
+    echo ----------
+    echo testsing $file
+    ./cub3D "$file" &
+    pid=$!
+    sleep 1
+    if kill -0 $pid 2>/dev/null; then
+        echo "cub3D is still running after 1 second. Terminating..."
+        kill $pid
+        wait $pid
+        EXIT_CODE=$?
+        echo "cub3D shouldn't be running with invalid map"
+        exit 1
+    else
+        wait $pid
+        EXIT_CODE=$?
+        echo "\ncub3D exited with exit code: $EXIT_CODE"
+        if $EXIT_CODE != 1; then
+            echo "wrong exit code"
+            exit 1
+        fi
+        echo "Success"
+    fi
+done
